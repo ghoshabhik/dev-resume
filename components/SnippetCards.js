534979a715c5678dd0e215ebe2b1701af3f412dc
abhik <avik5324@gmail.com>
2021-09-27 13:06:42 +0530

    SnippetLayoutUpdated

--- components/SnippetCards.js
+++ components/SnippetCards.js
@@ -3,7 +3,7 @@
 export default function SnippetCards() {
 
     return (
-        <div className="flex flex-col space-y-10 mb-10">
+        <div className="flex flex-col space-y-1 mb-10">
 
             <div className="text-2xl font-semibold ml-5">Snippets</div>
 
@@ -12,7 +12,9 @@ export default function SnippetCards() {
                 <a href="https://gist.github.com/ghoshabhik/c5497682fd07eb627b3fbff3e8d5776b" rel="nofollow noreferrer" target="_blank"
                     className="pl-5"
                 >
-                    <div className="ml-5 w-64 px-4 py-3 text-sm bg-white rounded-md">Python Automation to clean up disk space Somthing a little longer</div>
+                    <div className="ml-5 w-64 px-4 py-3 text-sm bg-white rounded-md shadow-xl border-l-8 border-blueaccent">
+                        Python <strong>Automation</strong> to clean up disk space Somthing a little longer
+                    </div>
                 </a>
             </div>
 
@@ -20,7 +22,7 @@ export default function SnippetCards() {
                 <a href="https://gist.github.com/ghoshabhik/c5497682fd07eb627b3fbff3e8d5776b" rel="nofollow noreferrer" target="_blank"
                     className="pl-5"
                 >
-                    <div className="ml-5 w-64 px-4 py-3 text-sm bg-white rounded-md">Project 2: Python Automation to clean up disk space Somthing a little longer</div>
+                    <div className="ml-5 w-64 px-4 py-3 text-sm bg-white rounded-md  shadow-xl border-l-8 border-blueaccent">Project 2: Python Automation to clean up disk space Somthing a little longer</div>
                 </a>
             </div>
 
@@ -28,7 +30,7 @@ export default function SnippetCards() {
                 <a href="https://gist.github.com/ghoshabhik/c5497682fd07eb627b3fbff3e8d5776b" rel="nofollow noreferrer" target="_blank"
                     className="pl-5"
                 >
-                    <div className="ml-5 w-64 px-4 py-3 text-sm bg-white rounded-md">Project3: Python Automation to clean up disk space Somthing a little longer</div>
+                    <div className="ml-5 w-64 px-4 py-3 text-sm bg-white rounded-md  shadow-xl border-l-8 border-blueaccent">Project3: Python Automation to clean up disk space Somthing a little longer</div>
                 </a>
             </div>
 
